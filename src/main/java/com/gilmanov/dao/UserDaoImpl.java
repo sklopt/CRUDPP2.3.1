955a25ff4a8d1dee05e6052f74efe1908a5e7a20
Shamil Gilmanov <sklopt@rambler.ru>
2023-05-03 15:52:23 +0500

    added support for Russian characters remove getEntityManager()

--- src/main/java/com/gilmanov/dao/UserDaoImpl.java
+++ src/main/java/com/gilmanov/dao/UserDaoImpl.java
@@ -6,42 +6,38 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
+
 @SuppressWarnings("unchecked")
 @Repository
 public class UserDaoImpl implements UserDao {
 
     @PersistenceContext
-    private EntityManager entityManager;
-
-    protected EntityManager getEntityManager() {
-        return this.entityManager;
-    }
+    private EntityManager em;
 
     @Override
     public List<User> getAllUsers() {
-        return getEntityManager().createQuery("From User order by 1").getResultList();
+        return em.createQuery("From User order by 1").getResultList();
     }
 
     @Override
     public void saveUser(User user) {
-        getEntityManager().persist(user);
+        em.persist(user);
     }
 
     @Override
     public void removeUserById(long id) {
-        getEntityManager()
-                .createQuery("delete from User where id=: id")
+        em.createQuery("delete from User where id=: id")
                 .setParameter("id", id)
                 .executeUpdate();
     }
 
     @Override
     public void changeUser(User user) {
-        getEntityManager().merge(user);
+        em.merge(user);
     }
 
     @Override
     public User getUserById(long id) {
-        return getEntityManager().find(User.class, id);
+        return em.find(User.class, id);
     }
 }
